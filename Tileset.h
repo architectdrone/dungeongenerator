3a11ee82a76130a743cb279fc77fb2e33111598a
Owen Mellema <owenmllm@gmail.com>
2018-10-01 23:16:21 -0500

    Add further details to Tileset.h

--- Tileset.h
+++ Tileset.h
@@ -1,11 +1,40 @@
 #ifndef TILESET_H
 #define TILESET_H
-
+#include "Tile.h"
+#include <iostream>
 class Tileset
 {
 private:
+	LinkedList<Tile*>* allTiles;
+
+public:
+	//CONSTRUCTOR
+	Tileset();
+
+	//DESTRUCTOR
+	~Tileset();
+
+	//GETTERS
+	LinkedList<Tile*>* getAllTiles();//Returns a pointer to the list of all tiles. Does NOT make a copy, so be careful.
+	Tileset* getAllWalls(); //Returns a tileset containing all walls within the original tileset.
+	Tileset* getAllNonWalls(); //Returns a tileset containing all non-walls within the original tileset.
+	Tileset* getAllX(int x); //Returns a tileset containing all tiles with the given x-coordinate within the original tileset.
+	Tileset* getAllY(int y); //Returns a tileset containing all tiles with the given y-coordinate within the original tileset.
+	Tile* getXY(int x, int y) throw(std::out_of_range); //Returns the tile with the given x and y coordinates. If none is found, throw an error.
+	Tile* getTile(int i); //Gets the ith tile. Remember, the first tile is at 0, the last is at len-1.
+	int getLength(); //Returns the length of the tileset.
+
+	//SETTERS
+	void setAllWall(); //Sets all tiles in the tileset to be walls.
+	void setAllNonWall(); //Sets all tiles in the tileset to be non-walls.
+
+	//TESTERS
+	bool isInTileset(Tile* myTile); //Tests to see whether or not the given tile is in the tileset.
+	bool isInTileset(int x, int y); //Tests to see whether or not the given coord is in the tileset.
+
+	//CLASS SPECIFIC
 	
 };
 
-#endif // !TILESET_H
+#endif
 
