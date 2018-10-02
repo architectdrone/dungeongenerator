6336454308af128fec9259f4981beb0a401c9ce0
Owen <owenmllm@gmail.com>
2018-10-01 21:44:25 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@ int main()
 {
 	cout << "Behold, the lamb of God!" << endl;
 	cout << "Woe, Woe, Woe!" << endl;
-	cout << "Death... this is truly terrifying...or is it?" << endl;
+	cout << "Death... this is truly terrifying...or is it? Good question" << endl;
 	return 0;
 }
