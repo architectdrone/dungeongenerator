d33eb1b05d28c8ad0a62ddeff46167840ea0e4d2
Owen Mellema <owenmllm@gmail.com>
2018-10-01 22:29:29 -0500

    Made main a test file

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
+#include "TIle.h"
 using namespace std;
 
 int main()
 {
-	cout << "Behold, the lamb of God!" << endl;
-	cout << "Woe, Woe, Woe!" << endl;
-	cout << "Death... this is truly terrifying...or is it? Good question" << endl;
-	cout << "YES BWA HAHAHAHAHAHAHA" << endl;
-	cout << "*cackles furiously*" << endl;
+	Tile myTile(3, 3);
 	return 0;
 }
