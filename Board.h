9945314f9ae2184ae13402f046cad463456deb3d
Owen Mellema <owenmllm@gmail.com>
2018-10-04 14:03:33 -0500

    Detail Board.h

--- Board.h
+++ Board.h
@@ -1,8 +1,28 @@
 #ifndef BOARD_H
 #define BOARD_H
+#include "Tileset.h"
+#include "Tile.h"
 
 class Board
 {
+private:
+	Tileset* boardTileset;
+	Tileset* outerWalls;
+	int x_size;
+	int y_size;
+
+public:
+	//CONSTRUCTORS
+	Board();
+
+	//DESTRUCTORS
+	~Board();
+
+	//GETTERS
+	Tileset* getAllTiles();
+
+	//CLASS SPECIFIC
+	void addOuterWalls();
 
 };
 
