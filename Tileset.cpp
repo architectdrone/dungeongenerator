25be3e0bbec471e104c57cb654b32eabf3a8e379
Owen Mellema <owenmllm@gmail.com>
2018-10-03 20:49:52 -0500

    Implement getAllY()

--- Tileset.cpp
+++ Tileset.cpp
@@ -80,6 +80,25 @@ Tileset* Tileset::getAllX(int x)
 	return toReturn;
 }
 
+Tileset* Tileset::getAllY(int y)
+{
+	Tileset* toReturn = new Tileset();
+
+	//Create iterator
+	list<Tile*>::iterator it = allTiles->begin();
+
+	//Do a for loop, stop when the pointer is the last one.
+	for (it; it != allTiles->end(); it++)
+	{
+		if (((*it)->getY()) == y)
+		{
+			toReturn->add(*it);
+		}
+	}
+	return toReturn;
+}
+
+
 void Tileset::add(Tile * newTile )
 {
 	if (!isInTileset(newTile))
