c06421d9c96f559254d7f32329d09a0c1f9aff29
Owen Mellema <owenmllm@gmail.com>
2018-10-03 19:23:56 -0500

    Implement the destructor

--- Tileset.cpp
+++ Tileset.cpp
@@ -1,9 +1,28 @@
 #include "Tileset.h"
 #include "Tile.h"
+#include <iterator>
+
 using namespace std;
 
 Tileset::Tileset()
 {
 	allTiles = new list<Tile*>;
+}
+
+Tileset::~Tileset()
+{
+	//We must delete all of the tiles individually, since they are merely pointers.
+
+	//Create iterator
+	list<Tile*>::iterator it = allTiles->begin();
+
+	//Do a for loop, stop when the pointer is the last one.
+	for (it; it != allTiles->end(); it++)
+	{
+		delete *it;
+	}
+
+	//Then delete the list
+	delete allTiles;
 }
 
