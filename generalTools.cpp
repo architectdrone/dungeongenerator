d7e04005db00a94103994c5ee459e7e3600d68e8
Owen Mellema <owenmllm@gmail.com>
2018-10-04 14:21:35 -0500

    Delete now depreciated createBoard from generalTools.cpp

--- generalTools.cpp
+++ generalTools.cpp
@@ -4,20 +4,3 @@
 #include <iostream>
 using namespace std;
 
-Tileset* createBoard(int x_size, int y_size) //Creates a board of size x_size*y_size
-{
-	Tileset* toReturn = new Tileset;
-	for (int y = 0; y < y_size; y++)
-	{
-		for (int x = 0; x < x_size; x++)
-		{
-			Tile* newTile = new Tile(x,y);
-			toReturn->add(newTile);
-		}
-	}
-}
-
-Tileset* createBoard(int size) //Creates a board of size size*size
-{
-	return createBoard(size, size);
-}
