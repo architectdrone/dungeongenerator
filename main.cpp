2eab7ef94bc254975651612d742ecee9e6e54e3a
Owen Mellema <owenmllm@gmail.com>
2018-10-04 14:45:25 -0500

    Delete generalTools.cpp, move printBoard() to main

--- main.cpp
+++ main.cpp
@@ -5,6 +5,31 @@
 #include "generalTools.cpp"
 using namespace std;
 
+void printBoard(Board* myBoard)
+{
+	int x_size = myBoard->getXSize();
+	int y_size = myBoard->getYSize();
+	Tile* currentTile;
+	Tileset* currentTileset = myBoard->getAllTiles();
+	string toPrint = "";
+	for (int y = 0; y < y_size; y++)
+	{
+		for (int x = 0; x < x_size; x++)
+		{
+			currentTile = currentTileset->getXY(x, y);
+			if (currentTile->getWall())
+			{
+				toPrint = toPrint + "#";
+			}
+			else
+			{
+				toPrint = toPrint + " ";
+			}
+		}
+		printf("%s\n", toPrint.c_str());
+	}
+}
+
 int main()
 {
 	Board myBoard(10);
