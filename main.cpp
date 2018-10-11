c5029e39230a316a03b5f29bb77eea2922879f23
Owen Mellema <owenmllm@gmail.com>
2018-10-10 19:54:37 -0500

    Removed the capability to generate neighboring straights in addStraights()

--- main.cpp
+++ main.cpp
@@ -183,18 +183,31 @@ void addStraights(Board* theBoard, int number)
 	Now, for example, consider the rightward-facing case. If I place a straight in the rightward direction, each wall will have a wall to it's left and to it's right. (except for the endpoints) Thus, we know it doesn't generate any edges in the rightward direction. However, the bottom of each tile is unaccounted for.
 	Thus, I would add each newly placed wall to the edges that were valid in the vertical direction.
 
+	Sidebar - Removing the capability for neighboring straights:
+	There was an algorithim error that involved the problem of neighboring straights. Essentially, two straights, when placed next to each other, caused some problems.
+	Fixing this, I determined, without changing the way the algorithim looks, would not only be a pain in the butt, but also put a tax on resource usage. Additionally, I didn't really like neighboring straights. See "optimized dungeon" in the readme.
+	So, I made a change to the output of the straights. Now, none of them will border. To do this, I removed from the edge-trackers any tile that would start a straight next to the current one.
+
 	Critical Assumptions, Limitations, and Algorithim Errors:
 	+ A very important assumption that limits the functionality of this algorithim is that the board must be assumed to only have outer-walls. Otherwise, we can't accept that every wall is an outerwall at the start
 		+ Easily fix this by using edgeTiles, but introduces some lag.
 	+ There is the problem of assuming that there isn't going to be any tiles below the ones placed (in the horizontal case)
+
+	TODO:
+	+ Fix memory leaks.
 	*/
 
 	bool horizontal = true;
 	Tileset horizontalEdges = edgeTiles(theBoard, true);
 	Tileset verticalEdges = edgeTiles(theBoard, false);
-	Tileset* currentEdges;
-	Tileset* nonCurrentEdges;
+	Tileset* currentEdges = nullptr;
+	Tileset* nonCurrentEdges = nullptr;
 	Tileset* newStraight;
+	Tileset* toRemoveFromCurrentEdges = nullptr;;
+	Tileset* toRemoveFromNonCurrentEdges = nullptr;;
+	//X and Y position of starting tile
+	int x;
+	int y;
 	for (int i = 0; i < number; i++)
 	{
 		//Toggle horizontal and vertical
@@ -219,7 +232,91 @@ void addStraights(Board* theBoard, int number)
 		//See algorithim notes. 
 		nonCurrentEdges->add(newStraight);
 
+		//Remove the capability to have straights next to each other (in theory)
+		x = myTile->getX();
+		y = myTile->getY();
+		toRemoveFromCurrentEdges = new Tileset;
+		toRemoveFromNonCurrentEdges = new Tileset;
+		if (horizontal)
+		{
+			
+			/*
+			#     #
+			C     #
+			CN###N#
+			C     #
+			#     #
+
+			C represents those things to be removed from the currentEdge
+			N represents those things to be removed from the nonCurrentEdge
+
+			C:
+			(x, y)
+			(x, y+1)
+			(x, y-1)
+			N:
+			(x+1, y)
+			({Largest X in straight tileset}, y)
+
+			Everything is wrapped in a try/catch block so that if it is out of range, nothing will happen.
+			*/
+			toRemoveFromCurrentEdges->add((theBoard->getAllTiles()->getXY(x, y)));
+			
+			if (theBoard->getAllTiles()->getMaxY() > y + 1)
+			{
+				toRemoveFromCurrentEdges->add((theBoard->getAllTiles()->getXY(x, y+1)));
+			}
+			if (theBoard->getAllTiles()->getMinY() < y - 1)
+			{
+				toRemoveFromCurrentEdges->add((theBoard->getAllTiles()->getXY(x, y - 1)));
+			}
+			if (theBoard->getAllTiles()->getMaxX() > x + 1)
+			{
+				toRemoveFromNonCurrentEdges->add((theBoard->getAllTiles()->getXY(x+1, y )));
+			}
+			toRemoveFromNonCurrentEdges->add(newStraight->getXY(newStraight->getMaxX(), y));
+		}
+		else
+		{
+			/*
+			#CCC#
+			  N  
+			  #
+			  N
+			#####
+			
+			C represents those things to be removed from the currentEdge
+			N represents those things to be removed from the nonCurrentEdge
+
+			C:
+			(x-1, y)
+			(x, y)
+			(x+1, y)
+			N:
+			(x, y+1)
+			(x, {Largest Y in Straight})
+			*/
+
+			toRemoveFromCurrentEdges->add((theBoard->getAllTiles()->getXY(x, y)));
+			if (theBoard->getAllTiles()->getMinX() < x-1)
+			{
+				toRemoveFromCurrentEdges->add((theBoard->getAllTiles()->getXY(x-1, y)));
+			}
+			if (theBoard->getAllTiles()->getMaxX() > x + 1)
+			{
+				toRemoveFromCurrentEdges->add((theBoard->getAllTiles()->getXY(x + 1, y)));
+			}
+			if (theBoard->getAllTiles()->getMaxY() > y+1)
+			{
+				toRemoveFromNonCurrentEdges->add((theBoard->getAllTiles()->getXY(x, y+1)));
+			}
+			toRemoveFromNonCurrentEdges->add(newStraight->getXY(x, newStraight->getMaxY()));
+		}
+		currentEdges->remove(toRemoveFromCurrentEdges);
+		nonCurrentEdges->remove(toRemoveFromNonCurrentEdges);
 	}
+
+	
 }
 
 int main()
